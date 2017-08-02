627c6b949cd33f1a9542da7f25d01d4fff76838c
kongtianxintang <kongtianxintang@163.com>
2017-08-02 16:40:31 +0800

    update:对Date类做的扩展 可以选择星期几 支持中文和英文

---
+++ Extension/OKDateExtension.swift
@@ -0,0 +1,76 @@
+/***********************************************************
+ * 版权所有,2017,MeFood.
+ * Copyright(C),2017,MeFood co. LTD.All rights reserved.
+ * project:Li
+ * Author:
+ * Date:  17/06/08
+ * QQ/Tel/Mail:
+ * Description:对日期类的扩展
+ * Others:
+ * Modifier:
+ * Reason:
+ *************************************************************/
+import UIKit
+
+
+protocol OKDateProtocol {
+    var weeks_en:Array<String> {get}
+    var weeks_cn:Array<String> {get}
+    var weeks_en_short:Array<String> {get}
+    var weekday_en:String {get}
+    var weekday_cn:String {get}
+    var weekday_en_short:String {get}
+}
+
+extension OKDateProtocol {
+    
+    var weeks_cn: Array<String> {
+        return ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"]
+    }
+    var weeks_en :Array<String> {
+        return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    }
+    var weeks_en_short:Array<String> {
+        return ["Sun.","Mon.","Tues.","Wed.","Thurs.","Fri.","Sat."];
+    }
+}
+
+extension Date:OKDateProtocol{
+    
+    var weekday_en_short: String {
+        let weekday = Calendar.current.component(.weekday, from: self);
+        return weeks_en_short[weekday - 1];
+    }
+
+    var weekday_cn: String {
+        let weekday = Calendar.current.component(.weekday, from: self);
+        return weeks_cn[weekday - 1];
+    }
+
+    var weekday_en: String {
+        let weekday = Calendar.current.component(.weekday, from: self);
+        return weeks_en[weekday - 1];
+    }
+}
+
+extension Date{
+    
+    //eg: 2017-03-15
+    var stringValue:String{
+        let formater = DateFormatter()
+        formater.dateFormat = "yyyy-MM-dd"
+        return formater.string(from: self)
+    }
+    
+    /*
+     eg:formatter = yyyy-MM-dd
+     return 2017-03-16
+     */
+    func stringValue(formatter:String)->String{
+        let formater = DateFormatter()
+        formater.dateFormat = formatter
+        return formater.string(from: self)
+    }
+    
+}
+
